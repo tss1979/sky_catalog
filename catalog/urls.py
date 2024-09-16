36a290e89966f20a43ca956b2ec5e6e6f9871f62
sergeytashkinov <tashkinov2@gmail.com>
2024-09-16 22:51:44 +0300

    done

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index, contacts, product_detail
+from .views import ProductListView, ProductDetailView, ContactsTemplateView
+
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index),
-    path('contacts/', contacts),
-    # path('product/<int:pk>', ProductDetail.as_view()),
-    path('product/<int:pk>', product_detail, name='product_detail'),
+    path('', ProductListView.as_view(), name='products'),
+    path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
+    path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,34 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, ListView, TemplateView
 
-from catalog.admin import ProductAdmin
 from catalog.models import Product
 
 
 # Create your views here.
+class ProductListView(ListView):
+    model = Product
+
+class ProductDetailView(DetailView):
+    model = Product
+    context_object_name = 'product'
+
+class ContactsTemplateView(TemplateView):
+    template_name = "catalog/contacts.html"
 
 
-def index(request):
-    context = {'object_list': Product.objects.all()}
-    return render(request, "catalog/home.html", context)
-
-def contacts(request):
-    if request.method == 'POST':
-        print({
-            'name': request.POST.get('name'),
-            'phone': request.POST.get('phone'),
-            'message': request.POST.get('message'),
-        })
-    return render(request, "catalog/contacts.html")
-
-# class ProductDetail(DetailView):
-#     model = Product
-#     template_name = 'catalog/product.html'
-#     context_object_name = 'product'
-
-
-def product_detail(request, pk):
-    template_name = 'catalog/product.html'
-    context = {'product': Product.objects.get(pk=pk)}
-    return render(request, template_name, context)
 
